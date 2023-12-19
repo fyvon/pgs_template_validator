26c4c4f9103c4f866883b51aefc24b0e23e737e6
Laurent Gil <lg10@sanger.ac.uk>
2023-12-19 15:03:47 +0000

    Check if a value belongs to a set of values

--- validator/score.py
+++ validator/score.py
@@ -3,10 +3,13 @@ from validator.efotrait import EFOTrait
 
 class Score():
 
+    genomebuilds = ['GRCh37','GRCh38','hg18','hg19','hg38','NCBI35','NCBI36']
+
     def check_data(self, fields_infos, mandatory_fields):
         validator = ScoreValidator(self, fields_infos, mandatory_fields)
         validator.check_not_null()
         validator.check_format()
+        validator.check_value('variants_genomebuild', self.genomebuilds)
         return validator.report
 
 
